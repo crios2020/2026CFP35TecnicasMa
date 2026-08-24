80992b9089605e1f6b38da7f5e5c5172cc4ae179
Daniel Ca <d.camina@bue.edu.ar>
2026-08-24 12:50:21 -0300

    Clase03

---
+++ Clase03/clase03.py
@@ -0,0 +1,46 @@
+#Linea de comentarios
+
+"""
+    Bloque 
+    de 
+    Comentarios
+"""
+
+# Lenguaje: Python          (Descargamos el interprete Python "python.org")
+
+# IDE (Integrated Development Enviroment) (Entorno de Desarrollo Integrado)
+# IDE: VsCode
+# Extensión: Python Extension Pack, Don Jayamanne
+
+print("Hola Mundo!") #Imprime en consola
+print("Hoy es lunes!")
+print("Centro de Formación Profesional Nro 35")
+print("Curso: Técnicas de programación")
+
+# Variables
+# Almacenan datos en memoria RAM
+
+#   Memoria RAM:        Volatil         Costosa(Cara)       Veloz       
+#   Disco Duro HD:      Persistente     Ecónomico           Lento
+
+#   Lenguajes de Tipados Fuerte: JAVA, C, C++, C#, Kotlin, Visual Basic
+#   Lenguajes de Tipados Debil:  Python, PHP, JavaScript
+
+# Declaración y asignación de variable
+#print(a)           # Error no esta declarada la variable
+a=2                 # asignamos un valor entero
+print(a)
+print("Variable a =",a)
+print(f"Variable a = {a}")          #Forma Optimizada
+
+# Azúcar sintáctico (syntactic sugar) - dos códigos distintos que hace lo mismo
+a=a+1           #Reasignación de valor
+print("Variable a =",a)
+
+#TODO
+# Extensión TODOTREE
+# Mutación de variable
+# Otros tipos de datos
+# Identificadores
+#  = operador de asignación
+
