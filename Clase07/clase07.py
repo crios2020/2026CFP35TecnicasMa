d5f3f478238c1acb20392d660076e3b9da49a162
Daniel Ca <d.camina@bue.edu.ar>
2026-09-07 12:44:05 -0300

    Clase07

---
+++ Clase07/clase07.py
@@ -0,0 +1,22 @@
+#Estructuras condicionales
+
+temperatura=10
+llueve=True
+
+#Estructura if
+if llueve:
+    print("Usar Paraguas!!")
+
+if llueve==True:
+    #código esta indentado
+    print("Esta lloviendo!!")
+    print("Usar Paraguas!!")
+
+if temperatura<=15:
+    print("Hace Frio!!!")
+
+print("---------------------------------------------------")
+if temperatura<=15 and llueve:
+    print("- Ideal para hacer tortafritas!!!")
+print("-- Fin del programa --")
+    
